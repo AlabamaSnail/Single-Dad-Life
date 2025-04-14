8d81b9597cbb72c624b9edd8f42fecaf53f8ff1d
AlabamaSnail <74316013+AlabamaSnail@users.noreply.github.com>
2025-04-14 12:43:20 -0500

    TALK TO NPC

--- phonetest/MainScene/Assets/Construction Man/constructionman.gd
+++ phonetest/MainScene/Assets/Construction Man/constructionman.gd
@@ -1,21 +1,32 @@
 extends Node3D
 
-@onready var interactLabel = self.get_parent().find_child("Label3D")
-
+@onready var interactLabel = $Label3D
+@onready var talkingmenu = $Talking
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("interact"):
+		if interactLabel.visible:
+			talkingmenu.visible = true
+			interactLabel.visible = false
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else: 
+			talkingmenu.visible = false
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "CharacterBody3D":
+
 		interactLabel.visible = true
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
 	if body.name == "CharacterBody3D":
 		interactLabel.visible = false
+		talkingmenu.visible = false
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
